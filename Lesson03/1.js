b5e038f43121e5fef82b74e3cc046d8cc685a264
Valerii_Panov <val250640@mail.ru>
2023-02-01 10:23:59 +1000

    HW03Task01

---
+++ Lesson03/1.js
@@ -0,0 +1,4 @@
+const temperC =   +prompt('Temperature C = ');
+const temperF = +1.8*  temperC + +32
+alert(`${temperC}C = ${temperF.toFixed(1)}F`)
+// console.log(`${temperC}C = ${Math.round(temperF)}F`)
